2455c3e5daa1955d912b0d3ebe91386a1bd02a22
JoeDE88 <joediaz88@hotmail.com>
2026-05-07 14:55:22 +0200

    bootstrap downloaded

--- spicy_coco/src/components/NavBar.jsx
+++ spicy_coco/src/components/NavBar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import eye from '../assets/eye.svg'
 
-export default function () {
+export default function NavBar() {
   return (
     <div >
-        Navbar
+      <nav class="navbar bg-secondary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src={eye} alt="Eye" width="30" height="24" class="d-inline-block align-text-top"/>
+            Bootstrap
+          </a>
+        </div>
+      </nav>
     </div>
 )
 }

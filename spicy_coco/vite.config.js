c805a6aac071bc9ef17573d3182c76880ca87dc6
JoeDE88 <joediaz88@hotmail.com>
2026-05-08 13:35:18 +0200

    added sass to override bootstrap, silenced deprecation warnings in vite config

--- spicy_coco/vite.config.js
+++ spicy_coco/vite.config.js
@@ -8,4 +8,11 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  css: {
+  preprocessorOptions: {
+    scss: {
+      silenceDeprecations: ["import"]
+    }
+  }
+}
 })

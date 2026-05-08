185dc5272fbdbd92eb4a2dc8b954080daa6e71b2
JoeDE88 <joediaz88@hotmail.com>
2026-05-08 13:38:55 +0200

    installed react routes and linked pages

--- spicy_coco/src/App.jsx
+++ spicy_coco/src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
+import {Routes, Route } from 'react-router'
+import Layout from './components/Layout'
 import NavBar from './components/NavBar'
+import { routesConfig } from './routes/routesConfig'
 
 function App() {
   return (
     <>
-    <NavBar></NavBar>
+    <Routes>
+      {routesConfig.map((page) => {
+        return (
+          <Route
+          key={page.name}
+          path={page.path}
+          element={page.component}
+          />
+        )
+      })}
+    </Routes>
     </>
   )
 }
--- spicy_coco/src/components/NavBar.jsx
+++ spicy_coco/src/components/NavBar.jsx
@@ -1,15 +1,39 @@
 import React from 'react'
 import eye from '../assets/eye.svg'
+import { NavLink } from 'react-router'
 
 export default function NavBar() {
   return (
     <div >
-      <nav class="navbar bg-secondary">
+      <nav class="navbar navbar-expand-lg bg-secondary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            <img src={eye} alt="Eye" width="30" height="24" class="d-inline-block align-text-top"/>
-            Bootstrap
-          </a>
+            <a class="navbar-brand" href="#">
+              <NavLink to="/">
+              <img src={eye} alt="Eye" width="30" height="24" class="d-inline-block align-text-top"/>
+              </NavLink>
+            </a>
+            <h1 className='text-primary'>
+                  Spicy Coco
+            </h1>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <NavLink className="nav-link" to="/quien-soy">Quien soy</NavLink>
+              </li>
+              <li class="nav-item">
+                <NavLink className="nav-link" to="/carta">Carta</NavLink>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contactos</a>
+              </li>
+            </ul>
+            <span class="navbar-text">
+                <a class="nav-link" href="#">Reservas</a>
+            </span>
+          </div>
         </div>
       </nav>
     </div>

59a9a2ddd75593b54c4eab485acb6a3aeadac370
JoeDE88 <joediaz88@hotmail.com>
2026-05-08 21:15:15 +0200

    Added simple cards with placeholders for plates

--- spicy_coco/src/pages/CartaPage.jsx
+++ spicy_coco/src/pages/CartaPage.jsx
@@ -4,7 +4,63 @@ export default function CartaPage(){
     return (
         <>
         <Layout>
-            <h1 class="text-primary">Esta es nuestra Carta</h1>
+            <h1 class="text-primary">Nuestros Platos</h1>
+            <div className="container">
+                <div className="row my-2">
+                    <div className="col-12">
+                        <div class="card mb-3" style={{maxwidth: "540px"}}>
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img src="https://placehold.co/600x400" class="img-fluid rounded-start" alt="..."/>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Nombre del Plato</h5>
+                                        <p class="card-text">Descripción del plato como ingredientes, preparación</p>
+                                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row my-2">
+                    <div className="col-12">
+                        <div class="card mb-3" style={{maxwidth: "540px"}}>
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img src="https://placehold.co/600x400" class="img-fluid rounded-start" alt="..."/>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Nombre del Plato</h5>
+                                        <p class="card-text">Descripción del plato como ingredientes, preparación</p>
+                                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row my-2">
+                    <div className="col-12">
+                        <div class="card mb-3" style={{maxwidth: "540px"}}>
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img src="https://placehold.co/600x400" class="img-fluid rounded-start" alt="..."/>
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Nombre del Plato</h5>
+                                        <p class="card-text">Descripción del plato como ingredientes, preparación</p>
+                                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Layout>
         </>
     )

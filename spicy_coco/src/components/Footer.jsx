0118b770aa162d8240dd811a56069b4e64a8c422
JoeDE88 <joediaz88@hotmail.com>
2026-05-08 21:14:15 +0200

    added insta, whatsapp and email icons, in rows and cols

--- spicy_coco/src/components/Footer.jsx
+++ spicy_coco/src/components/Footer.jsx
@@ -4,9 +4,14 @@ export default function Footer(){
         <footer className="bd-footer mt-3 py-3">
             <div className="container mt-3 py-3 px-3">
                 <div className="row">
-                    <div className="col-3">Footer</div>
-                    <div className="col-3">
+                    <div className="col-4 text-center">
                         <i class="bi bi-whatsapp"></i>
+                    </div>
+                    <div className="col-4 text-center">
+                        <i class="bi bi-instagram"></i>
+                    </div>
+                    <div className="col-4 text-center">
+                        <i class="bi bi-envelope"></i>
                     </div>
                 </div>
             </div>

bd67e9c87a5033488a0d9fc79572b98152dc232c
JoeDE88 <joediaz88@hotmail.com>
2026-05-11 11:57:46 +0200

    added h2 to footer

--- spicy_coco/src/components/Footer.jsx
+++ spicy_coco/src/components/Footer.jsx
@@ -1,16 +1,19 @@
 export default function Footer(){
     return (
         <>
-        <footer className="bd-footer mt-3 py-3">
-            <div className="container mt-3 py-3 px-3">
+        <footer className="bd-footer mt-2 py-3">
+            <div className="container mt-2 py-3 px-3">
+                <div className="row py-3">
+                    <h2 className="text-center text-secondary">Spicy Coco</h2>
+                </div>
                 <div className="row">
-                    <div className="col-4 text-center">
+                    <div className="col-4 text-center text-secondary">
                         <i class="bi bi-whatsapp"></i>
                     </div>
-                    <div className="col-4 text-center">
+                    <div className="col-4 text-center text-secondary">
                         <i class="bi bi-instagram"></i>
                     </div>
-                    <div className="col-4 text-center">
+                    <div className="col-4 text-center text-secondary">
                         <i class="bi bi-envelope"></i>
                     </div>
                 </div>

da04efce57680196a6f6f0f5416cc394e273ac1e
JoeDE88 <joediaz88@hotmail.com>
2026-05-12 20:07:16 +0200

    trying importing svgs to navbar

--- spicy_coco/src/components/NavBar.jsx
+++ spicy_coco/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import eye from '../assets/eye.svg'
+import logo from '../assets/logo.svg'
 import { NavLink } from 'react-router'
+import Button from './Button'
 
 export default function NavBar() {
   return (
@@ -8,12 +9,12 @@ export default function NavBar() {
       <nav class="navbar navbar-expand-lg bg-secondary">
         <div class="container-fluid">
               <NavLink to="/">
-              <img src={eye} alt="Eye" width="30" height="24" class="d-inline-block align-text-top"/>
+              <img src={logo} alt="Logo" width="100" height="100" class="d-inline-block align-text-top logo"/>
               </NavLink>
               <NavLink to="/" className='main-title-mobile'>
-            <h1 >Spicy Coco</h1>
+                <Button btn={<h1>Spicy Coco</h1>}></Button>
               </NavLink>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler btn-menu" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
               <i class="bi bi-list"></i>
           </button>
           <div class="collapse navbar-collapse" id="navbarText">
